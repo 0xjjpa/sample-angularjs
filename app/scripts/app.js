91d380bf3cb40d1bcd57ae06d15ac0369358b7c0
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-01-24 10:36:32 -0600

    Added angular template

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var AngularJSInitApp = angular.module('AngularJSInitApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
